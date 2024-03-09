5ff858cb6d2af011dc5366a2ffea4833c3cf8b12
CyberFinn <s>
2024-03-09 20:59:53 +0000

    added some weight checks to "add to inventory" functionality

--- Utils/Player Inventory/inventory_system.py
+++ Utils/Player Inventory/inventory_system.py
@@ -2,14 +2,19 @@
 inventory = []
 # Initialize the player's max weight (will make this change based on player details)
 max_weight = 25 # I'm just assuming 25kg is the total max the average human can carry throughout the day (items on you, etc.)
+# just doing this up here for code reusability, and so that we dont clutter the code
+too_heavy_message = "A magic back would help right about now! Can't carry any more!"
 
 # Function to add an item to the inventory
 def add_to_inventory(item):
-    if item not in inventory and not_overweight:
-        inventory.append(item)
-        print(f"Added {item} to your inventory.") #print(f"") is the python equivalent of C/C++/Go's printF functions - basically just does some formatting/interpolation
+    if not_overweight:
+        if item not in inventory:
+            inventory.append(item)
+            print(f"Added {item} to your inventory.") #print(f"") is the python equivalent of C/C++/Go's printF functions - basically just does some formatting/interpolation
+        else:
+            print(f"You already have {item} in your inventory.") # todo: need to be able to stack items
     else:
-        print(f"You already have {item} in your inventory.") # todo: need to be able to stack items
+         print(too_heavy_message)
 
 # Function to remove an item from the inventory. Returns true if removed, false if not removed (Item didnt exist)
 def remove_from_inventory(item_name):
@@ -25,12 +30,15 @@ def remove_from_inventory(item_name):
 
 # methods for adding stackable items to inventory (for stuff like potions, money, etc.)
 def add_to_stack(self):
-        if self.stack_count < self.max_stack_size:
-            self.stack_count += 1
-            print(f"Added {self.name} to your stack (total items: {self.stack_count}).")
+        if not_overweight:
+            if self.stack_count < self.max_stack_size:
+                self.stack_count += 1
+                print(f"Added {self.name} to your stack (total items: {self.stack_count}).")
+            else:
+                print(f"Your {self.name} stack is already full.")
+                #should probably just create a new stack in this case -> todo: revisit later
         else:
-            print(f"Your {self.name} stack is already full.")
-            #should probably just create a new stack in this case -> todo: revisit later
+             print(too_heavy_message)
 
 def remove_from_stack(self):
         if self.stack_count > 0:
